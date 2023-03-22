45e617bfcc040026329c88406970d16daf5223f6
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-03-22 16:53:03 +0530

    feat: ChainLink Price Oracle Consumer contract

--- contracts/chain-oracle/PriceConsumerV3.sol
+++ contracts/chain-oracle/PriceConsumerV3.sol
@@ -5,31 +5,21 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /// the Address list can be found for the pair
 contract PriceConsumerV3 {
-    AggregatorV3Interface internal priceFeed;
-
-    /**
-     * Network: Sepolia
-     * Aggregator: BTC/USD
-     * Address: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-     */
-    constructor() {
-        priceFeed = AggregatorV3Interface(
-            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-        );
-    }
+    constructor() {}
 
     /**
      * Returns the latest price.
      */
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice(
+        address pricePairAddress
+    ) public view returns (int) {
         // prettier-ignore
-        (
-            /* uint80 roundID */,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = priceFeed.latestRoundData();
+        ( /* uint80 roundID */,
+            int price ,
+          /*uint startedAt*/ ,
+          /*uint timeStamp*/  ,
+          /*uint80 answeredInRound*/
+        ) = AggregatorV3Interface(pricePairAddress).latestRoundData();
         return price;
     }
 }
