c4d0eb3ff5dd824e9ebc6f33c6ceed1b90028bc4
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-03-25 00:28:14 +0530

    feat: OOV3

--- backend/functions/OptimismOracleV3.js
+++ backend/functions/OptimismOracleV3.js
@@ -35,95 +35,123 @@ const OracleContractWithSigner = OracleContract.connect(wallet_Oracle);
 const ConsumerContractWithSigner = ConsumerContract.connect(wallet_Consumer);
 
 //1
-async function assertTruth(){
+async function assertTruthEventListner() {
   try {
-    console.log("listening to assert truth event")
-    OracleContract.on("assertTruth",(assertId,sender,claim,timestamp) => {
+    console.log("listening to assert truth event");
+    OracleContract.on("assertTruth", (assertId, sender, claim, timestamp) => {
       console.log("event emitted, asserted truth values:");
-      console.log(assertId,sender,claim,timestamp);
+      console.log(assertId, sender, claim, timestamp);
       storingAssertion();
-    })
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 //2
-async function storingAssertion(assertId,assertedClaim,requester){
+async function storingAssertion(assertId, assertedClaim, requester) {
   try {
-    console.log("storing assertion on node side")
-    const assertion = await ConsumerContractWithSigner.assertTruth(assertId,assertedClaim,requester)
+    console.log("storing assertion on node side");
+    const assertion = await ConsumerContractWithSigner.assertTruth(
+      assertId,
+      assertedClaim,
+      requester
+    );
     await assertion.wait();
-    console.log(assertion)
-    setAssertionId();
+    console.log(assertion);
+  } catch (error) {}
+}
+
+async function assertedEventListner() {
+  try {
+    console.log("listening to assert truth event");
+    ConsumerContract.on("truthAsserted", (assertId, assertionId) => {
+      console.log("event emitted, asserted truth values:");
+      console.log(assertId, assertionId);
+      setAssertionId(assertId, assertionId);
+    });
   } catch (error) {
-    
+    console.log(error);
   }
 }
 
 //3
-async function setAssertionId(assertId,assertionId){
+async function setAssertionId(assertId, assertionId) {
   try {
-    console.log("storing assertion id in oov3")
-    const id = await OracleContractWithSigner.setAssertionId(assertId,assertionId)
+    console.log("storing assertion id in oov3");
+    const id = await OracleContractWithSigner.setAssertionId(
+      assertId,
+      assertionId
+    );
     await id.wait();
-    console.log(id)
-    settleRequestListener();
+    console.log(id);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 //4
-async function settleRequestListener(){
+async function settleRequestListener() {
   try {
     console.log("listening for settle request event");
-    OracleContract.on("settleRequest",(assertId,timestamp) => {
-      console.log("event emitted")
-      console.log(assertId,timestamp);
-      settleRequest();
-    })
+    OracleContract.on("settleRequest", (assertId, timestamp) => {
+      console.log("event emitted");
+      console.log(assertId, timestamp);
+      settleRequest(assertId);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 //5
-async function settleRequest(){
+async function settleRequest(assertId) {
   try {
     console.log("settling the request using consumer");
-    const tx = await OracleContract.settleAndgetAssertionResult(assertId);
+    const tx = await ConsumerContractWithSigner.settleAndgetAssertionResult(
+      assertId
+    );
+    await tx.wait();
     console.log(tx);
-    storingresult();
+    // storingresult(assertId, assertResult);
+    gettingResult(assertId);
   } catch (error) {
     console.log(error);
   }
 }
 
 //6
-async function storingresult(){
+async function storingResult(assertId, assertResult) {
   try {
-    console.log("setting assert result in oov3")
-    const storeddata = await OracleContractWithSigner.setAssertResult(assertId,assertResult);
+    console.log("setting assert result in oov3");
+    const storeddata = await OracleContractWithSigner.setAssertResult(
+      assertId,
+      assertResult
+    );
     await storeddata.wait();
     console.log(storeddata);
     gettingResult();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 //7
-async function gettingResult(){
+async function gettingResult(assertId) {
   try {
-    console.log("getting result")
+    console.log("getting result");
     const data = await ConsumerContract.getAssertion(assertId);
     console.log(data);
+    storingResult(assertId, data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-} 
-
-function main(){
-  assertTruth();
 }
+
+async function main() {
+  assertTruthEventListner();
+  assertedEventListner();
+  settleRequestListener();
+}
+
+// main( )
