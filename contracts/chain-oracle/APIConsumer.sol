71137379267e442a8b1912f6a8daa6f906dc63bc
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-03-24 15:38:56 +0530

    feat: APIConsumer

---
+++ contracts/chain-oracle/APIConsumer.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/// Only for Single API call
+contract APIConsumer is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public volume;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    event RequestVolume(bytes32 indexed requestId, uint256 volume);
+
+    /**
+     * @notice Initialize the link token and target oracle
+     *
+     * Sepolia Testnet details:
+     * Link Token: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+     * Oracle: 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD (Chainlink DevRel)
+     * jobId: ca98366cc7314957b8c012c72f05aeeb
+     *
+     */
+    constructor(
+        address tokenAddress,
+        address oracleAddress,
+        string memory _jobId
+    ) ConfirmedOwner(msg.sender) {
+        setChainlinkToken(tokenAddress);
+        setChainlinkOracle(oracleAddress);
+        jobId = _jobId;
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+    }
+
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
+    function requestData(
+        string memory apiLink,
+        string memory apiPath
+    ) public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add("get", apiLink);
+        req.add("path", apiPath); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10 ** 18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+    function fulfill(
+        bytes32 _requestId,
+        uint256 _volume
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestVolume(_requestId, _volume);
+        volume = _volume;
+    }
+
+    /**
+     * Allow withdraw of Link tokens from the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
--- contracts/client-oracle/PriceOracleV1.sol
+++ contracts/client-oracle/PriceOracleV1.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.8.14;
 
 // Deployed on the new chain
 contract PriceOracleV1 {
-    mapping(address => uint) public pricePair;
+    mapping(address => uint) public pricePairs;
+
+    event requestPrice(address pricePair);
+
+    function requestPairPrice(address pricePair) public {
+        emit requestPrice(pricePair);
+    }
 
     //Need to put up restrictions
     function setLatestPrice(address pairAddress, uint256 _price) public {
-        pricePair[pairAddress] = _price;
+        pricePairs[pairAddress] = _price;
     }
 
     //Get the latest Price
     function getLatestPrice(address pairAddress) public view returns (uint) {
-        return pricePair[pairAddress];
+        return pricePairs[pairAddress];
     }
 }
