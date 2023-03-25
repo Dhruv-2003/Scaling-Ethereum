828ecd4a5b6153ff7af1411e98f56162b1dc1815
Architsharma7 <architsharma7911@gmail.com>
2023-03-25 12:25:14 +0530

    fix: VRFOracle functions completed

--- backend/functions/VRFOracleV2.js
+++ backend/functions/VRFOracleV2.js
@@ -34,3 +34,111 @@ const ConsumerContract = new ethers.Contract(
 const OracleContractWithSigner = OracleContract.connect(wallet_Oracle);
 const ConsumerContractWithSigner = ConsumerContract.connect(wallet_Consumer);
 
+async function requestRandomnessListener() {
+  try {
+    console.log("listening to request randomness event");
+    OracleContract.on(
+      "randomnessRequested",
+      (vrfId, numWords, requestConfirmations) => {
+        console.log("event emitted");
+        console.log(vrfId, numWords, requestConfirmations);
+        requestRandomness();
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function requestRandomness(numWords, requestConfirmations) {
+  try {
+    console.log("requesting random");
+    const random = await OracleContractWithSigner.requestRandomness(
+      numWords,
+      requestConfirmations
+    );
+    await random.wait();
+    console.log(random);
+    requestRandomWords();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function requestRandomWords(vrfId, numWords, requestConfirmations) {
+  try {
+    console.log("adding details to consumer contract");
+    const tx = await ConsumerContractWithSigner.requestRandomWords(
+      vrfId,
+      numWords,
+      requestConfirmations
+    );
+    await tx.wait();
+    console.log(tx);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function requestSentListener() {
+  try {
+    console.log("listening to request sent event");
+    ConsumerContract.on(
+      "RequestSent",
+      (requestId, vrfId, numWords, paidAmount) => {
+        console.log("event emitted");
+        console.log(requestId, vrfId, numWords, paidAmount);
+        setRequestData();
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function setRequestData(vrfId, requestId, paidAmount) {
+  try {
+    console.log("setting data into oracle");
+    const tx = await OracleContractWithSigner.setRequestData(
+      vrfId,
+      requestId,
+      paidAmount
+    );
+    await tx.wait();
+    console.log(tx);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function requestFulfilListener() {
+  try {
+    console.log("listeneing to request fulfil event")
+    ConsumerContract.on(
+      "RequestFulfilled",
+      (vrfId, _requestId, _randomWords, s_requests) => {
+        console.log("event emitted")
+        console.log(vrfId, _requestId, _randomWords, s_requests)
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function fulfillRandomness(vrfId,_randomWords){
+    try {
+        console.log("calling fulfilrandomness from oracle")
+        const fulran = await OracleContractWithSigner.fulfillRandomness(vrfId,_randomWords);
+        await fulran.wait();
+        console.log(fulran)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function main() {
+  requestRandomnessListener();
+  requestSentListener();
+  requestFulfilListener();
+}
